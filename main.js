f2830244e1563d7960992fa4b0a568d9a44c49f3
Kyle Whynott <kyle.whynott@gmail.com>
2024-10-29 21:17:30 -0700

    Finished Intial Configuration and Set-Up Project Structure

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ const {app, BrowserWindow, Menu} = require('electron');
 const { updateElectronApp } = require('update-electron-app');
 updateElectronApp();
 Menu.setApplicationMenu(null);
-//const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        //autoHideMenuBar: true
-        /*webPreferences {
-            preload: path.join(_dirname, 'preload.js')
-        }*/
+        autoHideMenuBar: true,
     })
 
     win.loadFile('index.html')
